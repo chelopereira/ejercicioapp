90cc02af631ad12c1848b3b80000ef6bedcdae33
chelopereira <chelopp87@gmail.com>
2016-12-19 12:32:45 -0500

    Adding Carrierwave

--- config/environment.rb
+++ config/environment.rb
@@ -4,10 +4,10 @@ require_relative 'application'
 # Initialize the Rails application.
 Rails.application.initialize!
 ActionMailer::Base.smtp_settings = {
-  :user_name => 'probandocorreo87@gmail.com', #usuario que utilizamos para enviar el correo
-  :password => 'abcabc87', #contraseña de la cuenta
-  :domain => 'gmail.com',
-  :address => 'smtp.gmail.com',
-  :port => 587, # puerto que siempre se utiliza
-  :authentication => :plain,
-  :enable_starttls_auto => true }
+ :user_name => 'probandocorreo87@gmail.com',
+ :password => "abcabc87",
+ :domain => 'gmail.com',
+ :address => 'smtp.gmail.com',
+ :port => 587,
+ :authentication => :plain,
+ :enable_starttls_auto => true }
